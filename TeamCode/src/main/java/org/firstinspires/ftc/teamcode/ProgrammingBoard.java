85db9f81d70194231f69e3d13101efbfd6e8134a
AltruisticEagle <jasonfang100886@gmail.com>
2026-09-02 11:32:37 -0400

    board update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoard.java
@@ -1,65 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.AnalogInput;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class ProgrammingBoard {
-    private DigitalChannel touchSensor;
     private DcMotor motor;
-    private double ticksPerRotation;
-    private Servo servo;
-    private AnalogInput pot;
-    private ColorSensor colorSensor;
-    private DistanceSensor distanceSensor;
 
     public void init(HardwareMap hardwareMap) {
-        touchSensor = hardwareMap.get(DigitalChannel.class, "touch_sensor");
-        touchSensor.setMode(DigitalChannel.Mode.INPUT);
-
         motor = hardwareMap.get(DcMotor.class, "motor");
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        servo = hardwareMap.get(Servo.class, "servo");
-        pot = hardwareMap.get(AnalogInput.class, "pot");
-
-        colorSensor = hardwareMap.get(ColorSensor.class, "sensor_color_distance");
-        distanceSensor = hardwareMap.get(DistanceSensor.class, "sensor_color_distance");
     }
 
-    public boolean isTouchSensorPressed() {
-        return touchSensor.getState();
-    }
 
     public void setMotorSpeed(double speed) {
         motor.setPower(speed);
     }
-
-    public double getMotorRotations() {
-        return motor.getCurrentPosition() / ticksPerRotation;
-    }
-
-    public void setServoPosition(double position) {
-        servo.setPosition(position);
-    }
-
-    public double getPotAngle() {
-        return Range.scale(pot.getVoltage(), 0, pot.getMaxVoltage(), 0,270);
-    }
-
-    public int getAmountRed() {
-        return colorSensor.red();
-    }
-
-    public double getDistance (DistanceUnit distanceUnit) {
-        return distanceSensor.getDistance(distanceUnit);
-    }
-
 }
+
