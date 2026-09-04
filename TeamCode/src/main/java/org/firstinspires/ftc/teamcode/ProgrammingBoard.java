e923a6163b48885aaa14fa224b833851ff89d035
AltruisticEagle <jasonfang100886@gmail.com>
2026-09-04 09:52:46 -0400

    v1 starter bot opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingBoard.java
@@ -2,20 +2,60 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class ProgrammingBoard {
-    private DcMotor motor;
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor rearLeftMotor;
+    private DcMotor rearRightMotor;
+    private DcMotor intakeMotor;
+    private Servo leftIntakeServo;
+    private Servo rightIntakeServo;
+
 
     public void init(HardwareMap hardwareMap) {
-        motor = hardwareMap.get(DcMotor.class, "motor");
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // There are 7 devices on the starter bot at the moment
+        // 5 DC Motors
+        // 4 of them for wheels and 1 for the intake
+        // 2 Servos, one on either side of the intake
+
+        // Initialising wheel motors
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rearLeftMotor = hardwareMap.get(DcMotor.class, "rearLeftMotor");
+        rearLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rearRightMotor = hardwareMap.get(DcMotor.class, "rearRightMotor");
+        rearRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Initialising intake motor
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Initialising intake servos
+        leftIntakeServo = hardwareMap.get(Servo.class, "leftIntakeServo");
+
+        rightIntakeServo = hardwareMap.get(Servo.class, "rightIntakeServo");
     }
 
+    public void setRobotSpeed(double speed) {
+        frontLeftMotor.setPower(speed);
+        frontRightMotor.setPower(speed);
+        rearLeftMotor.setPower(speed);
+        rearRightMotor.setPower(speed);
+    }
 
-    public void setMotorSpeed(double speed) {
-        motor.setPower(speed);
+    public void intake() {
+        intakeMotor.setPower(0.5);
+        leftIntakeServo.setPosition(0);
+        rightIntakeServo.setPosition(0);
     }
 }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/starterBotOpMode_Sept.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class starterBotOpMode_Sept extends OpMode {
+    ProgrammingBoard board = new ProgrammingBoard();
+    // private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        double speed = gamepad1.left_stick_y;
+        boolean intake = gamepad1.a;
+
+        if (speed != 0) {
+            board.setRobotSpeed(speed);
+        }
+        if (intake) {
+            board.intake();
+        }
+    }
+}
